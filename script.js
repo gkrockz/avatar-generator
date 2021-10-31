44d16e82f444a165bf8d5c48a49f3b0e0b1d22c3
gkrockz <61354468+gkrockz@users.noreply.github.com>
2021-10-31 19:04:51 +0530

    minor fixes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var userInput = document.getElementById("name");
+var avatar = document.getElementById("avatar");
+var downloadLink = document.getElementById("imgsrc");
+var usrname = document.getElementById("usrname");
+var gender = document.getElementById("gender");
 
-// returns gender with the help of API Call, based on userName provided 
 async function fetchGender(userName) {
     try {
         const response = await fetch(`https://api.genderize.io?name=${userName}`);
@@ -12,29 +15,36 @@ async function fetchGender(userName) {
     }
 }
   
-// Generate avatar  
 async function generateAvatar(userName) {
-    const response = await fetchGender(userName);
-    var avatar = document.getElementById("avatar");
-    var downloadLink = document.getElementById("imgsrc");
+    
+    const response = await fetchGender(userName); 
+    avatar.style.display = "initial";
     var apiUrl = `https://avatars.dicebear.com/api/${response.gender}/${userName}.svg?b=%235e728d&r=10?mood[]=happy`;
     avatar.src = apiUrl;
-    document.getElementById("usrname").textContent = `Name: ${userName}`;
-    document.getElementById("gender").textContent = `Gender: ${response.gender}`;
+    usrname.textContent = `Name: ${userName}`;
+    gender.textContent = `Gender: ${response.gender}`;
     downloadLink.href = apiUrl;
     downloadLink.style.display="inherit";
-    avatar.onerror = function () {
-      downloadLink.style.display = "none";      
-    };
+
+    avatar.onerror = () => {
+      console.clear();
+      avatar.style.display = "none";
+      downloadLink.style.display = "none";
+      gender.textContent = "Sorry, no avatar found 😄";
+    }
     userInput.value="";
-} 
+  }
 
 // invokes driver function  
 function invokeFunc() {
-    generateAvatar(userInput.value.toLowerCase());
+    if(userInput.value === "") {
+      alert("oops, you forgot to enter your name !");
+    }
+    else {
+      userInput.value.length > 2 ? generateAvatar(userInput.value.toLowerCase()) : alert("please enter a valid name !");
+    }
   }
-    
-// Function for key-press event
+
 function EventForKeyPress(event) {
   if (event.keyCode === 13) {
     invokeFunc();
@@ -42,5 +52,3 @@ function EventForKeyPress(event) {
 }
 
 userInput.addEventListener("keypress", EventForKeyPress);
-
-
